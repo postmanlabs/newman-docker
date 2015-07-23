5dd8a83dba45badbf40e76f9470b849c2a8eaa1c
Aniket Panse <aniket@getpostman.com>
2015-07-23 13:54:35 +0530

    use a better validator that is more configurable

--- tests/dockerfiles-spec.js
+++ tests/dockerfiles-spec.js
@@ -5,20 +5,25 @@
 var expect = require('expect.js'),
     fs = require('fs'),
     path = require('path'),
-    validateDockerfile = require('validate-dockerfile');
+    DockerFileValidator = require('dockerfile_lint');
 
 /* global describe, it */
 describe('Validate Dockerfiles', function () {
-    var versions = ['ubuntu_14_04']; // TODO: Decide how we want to automatically load versions.
+    var versions = ['ubuntu_14_04'],
+        rules = fs.readFileSync(path.join(__dirname, 'dockerfile_rules.yml')),
+        validator = new DockerFileValidator(rules); // TODO: Decide how we want to automatically load versions.
+
     versions.map(function (version) {
         var dockerFilePath = path.join(__dirname, '..', version, 'Dockerfile'),
             dockerFileContent = fs.readFileSync(dockerFilePath);
+
         it('Docker file for "' + version + '" must be valid', function () {
-            var result = validateDockerfile(dockerFileContent.toString());
-            if (!result.valid) {
+            var result = validator.validate(dockerFileContent.toString()),
+                numBadThings = result.error.count + result.warn.count;
+            if (!(numBadThings == 0)) {
                 console.log(JSON.stringify(result, null, 4)); // Helps debugging on the CI
             }
-            expect(result.valid).to.be(true);
+            expect(numBadThings).to.be(0);
         });
     });
 });
